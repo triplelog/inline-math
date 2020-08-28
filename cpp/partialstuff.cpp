3b785d2944459ca8a6a839e797600bc38f9dbec2
triplelog <robmwilc@gmail.com>
2020-08-28 08:47:32 -0400

    nm

--- cpp/partialstuff.cpp
+++ cpp/partialstuff.cpp
@@ -33,9 +33,9 @@ using phmap::flat_hash_map;
 #include "solve.cpp"
 
 std::vector<std::vector<Step>> partialTree(std::string pfstr){
-	map<std::string,std::vector<std::string>> listMap;
-	map<int,int> operandMap;
-	map<int,std::string> originalMap;
+	std::map<std::string,std::vector<std::string>> listMap;
+	std::map<int,int> operandMap;
+	std::map<int,std::string> originalMap;
     std::vector<Step> returnStringsCorrect;
     std::vector<Step> returnStringsIncorrect;
 	int i; int ii; int iii;
