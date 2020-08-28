a8d5fc1bd6f24765aced2e7c2f25e80c70c3596c
triplelog <robmwilc@gmail.com>
2020-08-28 19:51:05 -0400

    nm

--- cpp/graphing.cpp
+++ cpp/graphing.cpp
@@ -5,7 +5,7 @@ std::string convertCoordinates(double x,double  y,double left, double right,doub
 		else{xstr = xstr.substr(0,5);}
 		if (y>=bottom && y<=top){
 			std::string ystr = std::to_string(100-100*(y-bottom)/(top-bottom));
-			if (ystr.at(0) == "-"){ystr = ystr.substr(0,6);}
+			if (ystr.at(0) == '-'){ystr = ystr.substr(0,6);}
 			else{ystr = ystr.substr(0,5);}
 			return xstr + " " + ystr;
 		}
@@ -31,7 +31,7 @@ std::string convertX(double x,double left, double right,double shift){
 std::string convertY(double y,double bottom,double top,double shift){
 	if (y>=bottom && y<=top){
 		std::string ystr = std::to_string(100-100*(y-bottom)/(top-bottom));
-		if (ystr.at(0) == "-"){ystr = ystr.substr(0,6);}
+		if (ystr.at(0) == '-'){ystr = ystr.substr(0,6);}
 		else{ystr = ystr.substr(0,5);}
 		return ystr;
 	}
@@ -52,26 +52,26 @@ std::string makeGraph(std::string fn){
 			if (top-bottom>5){
 				for (ii=floor(bottom)+1;ii<floor(top)+1;ii++){
 					if (i%5==0 && ii%5 == 0){
-						svg += "<circle cx=\""+convertX(i,left,right)+"\" cy=\""+convertY(ii,bottom,top)+"\" r=\".6\" fill=\"red\"/>";
+						svg += "<circle cx=\""+convertX(i,left,right,0)+"\" cy=\""+convertY(ii,bottom,top,0)+"\" r=\".6\" fill=\"red\"/>";
 					}
 					else if (i%5==0 || ii%5 == 0){
-						svg += "<circle cx=\""+convertX(i,left,right)+"\" cy=\""+convertY(ii,bottom,top)+"\" r=\".4\" fill=\"red\"/>";
+						svg += "<circle cx=\""+convertX(i,left,right,0)+"\" cy=\""+convertY(ii,bottom,top,0)+"\" r=\".4\" fill=\"red\"/>";
 					}
 					else {
-						svg += "<circle cx=\""+convertX(i,left,right)+"\" cy=\""+convertY(ii,bottom,top)+"\" r=\".2\" fill=\"red\"/>";
+						svg += "<circle cx=\""+convertX(i,left,right,0)+"\" cy=\""+convertY(ii,bottom,top,0)+"\" r=\".2\" fill=\"red\"/>";
 					}
 					
 				}
 			}
 			
 			if (i%5 == 0 && i != 0){
-				svg += "<circle cx=\""+convertX(i,left,right)+"\" cy=\""+convertY(0,bottom,top,3)+"\" r=\"4px\" fill=\"white\"/>";
+				svg += "<circle cx=\""+convertX(i,left,right,0)+"\" cy=\""+convertY(0,bottom,top,3)+"\" r=\"4px\" fill=\"white\"/>";
 				svg += "<text fill=\"black\" font-size=\"6px\" text-anchor=\"middle\" dominant-baseline=\"hanging\" x=\""+convertX(i,left,right)+"\" y=\""+convertY(0,bottom,top,1)+"\">&nbsp;"+i+"&nbsp;</text>";
 			}
 		}
 		for (var ii=floor(bottom)+1;ii<floor(top)+1;ii++){
 			if (ii%5 == 0 && ii != 0){
-				svg += "<circle cx=\""+convertX(0,left,right,-3)+"\" cy=\""+convertY(ii,bottom,top)+"\" r=\"4px\" fill=\"white\"/>";
+				svg += "<circle cx=\""+convertX(0,left,right,-3)+"\" cy=\""+convertY(ii,bottom,top,0)+"\" r=\"4px\" fill=\"white\"/>";
 				svg += "<text font-size=\"6px\" text-anchor=\"end\" dominant-baseline=\"middle\" x=\""+convertX(0,left,right,-1)+"\" y=\""+convertY(ii,bottom,top,1)+"\">"+ii+"</text>";
 			}	
 		}
@@ -84,7 +84,7 @@ std::string makeGraph(std::string fn){
 	
 	std::vector<double> points = getPoints(fn,"x",left,right);
 	//console.log(outStr);
-	for (var i=0;i<1001;i++){
+	for (i=0;i<1001;i++){
 		path += convertCoordinates(points[i*2],points[i*2+1],left,right,bottom,top)+ " ";
 	}
 	svg += "<path d=\""+path+"\" stroke=\"rgb(60,60,60)\" fill=\"none\"/>";
