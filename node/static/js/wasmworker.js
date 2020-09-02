a0b78a9a7bc32ee9fd606895a2079d9c57350fc1
triplelog <robmwilc@gmail.com>
2020-09-02 19:05:57 -0400

    nm

--- node/static/js/wasmworker.js
+++ node/static/js/wasmworker.js
@@ -70,6 +70,7 @@ const renderer = {
 	if (matchLower && matchLower.index == 0){
 		if (matchLower[0].search('$p')>0){
 			svg = "<span>";
+			console.log(match[1].trim());
 			p(match[1].trim(),-10,10,-10,10);
 			svg += '<br><input type="range" id="domainSlider" min="0" max="'+(20*2)+'" value="'+20+'"></input>';
 			svg += '</span>';
