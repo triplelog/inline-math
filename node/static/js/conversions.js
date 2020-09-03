bba942ecb6156b1e98c02b10b223627b990ea799
triplelog <robmwilc@gmail.com>
2020-09-03 17:38:35 -0400

    nm

--- node/static/js/conversions.js
+++ node/static/js/conversions.js
@@ -1,6 +1,6 @@
 function jsToMath(input){
 	console.log(input);
-	input = input.replace(/math\.(.{1,6})\(/gi,'$1');
+	input = input.replace(/math\.(.{1,6})\(/gi,'$1(');
 	console.log(input);
 	return input;
 }
