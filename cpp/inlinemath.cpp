c7f5a789c0dabbd5f222e8936c173c324c7732ef
triplelog <robmwilc@gmail.com>
2020-09-04 14:53:38 -0400

    nm

--- cpp/inlinemath.cpp
+++ cpp/inlinemath.cpp
@@ -167,7 +167,7 @@ void LatexIt(char* aa) {
 	
 	//std::string noIdentities = doCalculus(postfixed);
 	postfixed = "\0";
-	std::string latexed = latexOne(noIdentities);
+	std::string latexed = latexOne(postfixed);
 	noIdentities = "\0";
 	latexed += "\0";
 	output_latex(latexed.c_str());
