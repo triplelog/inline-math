32b0b6ad3f080a5119f79871b0074698d097102f
triplelog <robmwilc@gmail.com>
2020-09-07 07:49:39 -0400

    nm

--- node/static/js/wasmworker.js
+++ node/static/js/wasmworker.js
@@ -241,6 +241,7 @@ const renderer = {
 				tree.nodes[node.substr(0,node.length-1)+"o"].parent = node;
 			}
 		}
+		console.log(tree);
 		var outText = "";
 		for (var i=0;i<tree.allNodes.length;i++){
 			var node = tree.allNodes[i];
