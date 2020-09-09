7523d4ab89333e897cfeb439287edb85780b6160
triplelog <robmwilc@gmail.com>
2020-09-08 20:00:43 -0400

    nm

--- node/graphics/makefavicon.js
+++ node/graphics/makefavicon.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var svg = "<svg version=\"1.1\" baseProfile=\"full\" viewBox=\"-100 -100 100 100\" width=\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">";
 
 for (var i=0;i<100;i++){
