50c8930d4fb5e918f1ca2194d53f073890e08521
triplelog <robmwilc@gmail.com>
2020-09-14 10:04:42 -0400

    nm

--- node/static/js/wasmworker.js
+++ node/static/js/wasmworker.js
@@ -379,10 +379,12 @@ const renderer = {
 					}
 				}
 			}
+			cosole.log(fn);
 			svg = '<span class=="plotSpan" id="plot-'+0+'" data-formula="'+fn+'" data-left="'+left+'" data-right="'+right+'" data-bottom="'+bottom+'" data-top="'+top+'" >';
 			//pjs(fn,left,right,bottom,top);
 			//svg += '<br><input type="range" data-formula="'+fn+'" id="domainSlider-'+0+'" min="0" max="'+((right-left)*2)+'" value="'+(right-left)+'"></input>';
 			svg += '</span>';
+			console.log(svg);
 			return svg;
 		}
 		else if (input.search(/tree\(/)==0){
