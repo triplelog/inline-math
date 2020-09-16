1b52c8b0a82ff3f7b6e1bd98e4ab45c6380b5f44
triplelog <robmwilc@gmail.com>
2020-09-16 18:53:26 -0400

    nm

--- node/graphics/makefavicon.js
+++ node/graphics/makefavicon.js
@@ -42,6 +42,52 @@ for (var i=0;i<100;i++){
 svg += "</svg>";
 fs.writeFileSync("../node/static/favicon.html", svg);
 
+
+var svg = "<svg version=\"1.1\" baseProfile=\"full\" viewBox=\"-128 -128 128 128\" width=\"600\" height=\"600\" xmlns=\"http://www.w3.org/2000/svg\">";
+
+for (var i=0;i<129;i++){
+	for (var ii=0;ii<129;ii++){
+		var r = .06;
+		if (i%2==0){
+			r*=1.5;
+		}
+		if (ii%2==0){
+			r*=1.5;
+		}
+		if (i%4==0){
+			r*=1.5;
+		}
+		if (ii%4==0){
+			r*=1.5;
+		}
+		if (i%8==0){
+			r*=1.5;
+		}
+		if (ii%8==0){
+			r*=1.5;
+		}
+		if (i%16==0){
+			r*=1.5;
+		}
+		if (ii%16==0){
+			r*=1.5;
+		}
+		//svg += "<circle cx=\"" + i + "\" cy=\"" + ii + "\" r=\""+(r*1.7)+"\" fill=\"#5381AC\"/>";
+		//svg += "<circle cx=\"" + i + "\" cy=\"-" + ii + "\" r=\""+(r*1.7)+"\" fill=\"#5381AC\"/>";
+		//svg += "<circle cx=\"-" + i + "\" cy=\"" + ii + "\" r=\""+(r*1.7)+"\" fill=\"#5381AC\"/>";
+		//svg += "<circle cx=\"-" + i + "\" cy=\"-" + ii + "\" r=\""+(r*1.7)+"\" fill=\"#5381AC\"/>";
+		
+		svg += "<circle cx=\"" + i + "\" cy=\"" + ii + "\" r=\""+r+"\" fill=\"#AC6053\"/>";
+		svg += "<circle cx=\"" + i + "\" cy=\"-" + ii + "\" r=\""+r+"\" fill=\"#AC6053\"/>";
+		svg += "<circle cx=\"-" + i + "\" cy=\"" + ii + "\" r=\""+r+"\" fill=\"#AC6053\"/>";
+		svg += "<circle cx=\"-" + i + "\" cy=\"-" + ii + "\" r=\""+r+"\" fill=\"#AC6053\"/>";
+	}
+}
+svg += "</svg>";
+fs.writeFileSync("../node/static/bg.html", svg);
+
+
+
 var svg = `<!DOCTYPE html>
 <html>
 
