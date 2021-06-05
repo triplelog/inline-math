a5e96fcdbbc80b0baeed30b2873c2df669d204f7
triplelog <robmwilc@gmail.com>
2021-06-05 08:54:27 -0400

    nm

--- cpp/postfixify.cpp
+++ cpp/postfixify.cpp
@@ -1292,7 +1292,34 @@ std::vector<std::string> postfixifyVector(std::string input_str, bool checkCompu
 
 	infixexpr[input_str.length()] = '\0';
 	
-
+	bool showFAM = false;
+	
+	if (followAMap["original"].length() > 2){
+		for (iii=0;iii<followAMap["original"].length();iii++){
+			if (followAMap["original"].at(iii) != '0'){
+				showFAM = true;
+				break;
+			}
+		}
+	}
+	if (showFAM){
+		std::string visOriginal = "";
+		std::string visNew = "";
+		for (iii=0;iii<followAMap["original"].length();iii++){
+			int s000 = followAMap["original"].at(iii);
+			visOriginal += std::to_string(s000)+",";
+		}
+		for (iii=0;iii<followAMap["expstr"].length();iii++){
+			int s000 = followAMap["expstr"].at(iii);
+			visNew += std::to_string(s000)+",";
+		}
+		string_log("NonZero1");
+		string_log(visOriginal.c_str());
+		string_log(visNew.c_str());
+		string_log(infixexpr);
+		string_log(postVector[0].c_str());
+	}
+	
 	std::vector<std::string> postVector = makePostVector(infixexpr);
 	//std::cout <<"pv: "<< postVector[0] << " and " << postVector[1] << "\n";
 
